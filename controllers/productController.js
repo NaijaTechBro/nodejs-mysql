dbdea3eae037d8f58830ef64c694cb4c77b02172
NaijaTechBro <bakisodiq@gmail.com>
2023-05-08 11:48:58 +0100

    controller --productw

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,5 +9,14 @@ const Review = db.reviews
 // create product
 
 const addProduct = async (req, res) => {
-    
+
+    let info = {
+        title: req.body.title,
+        price: req.body.price,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    }
+
+    const product = await Product.create(info)
+    res.status(200).send(product)
 }
