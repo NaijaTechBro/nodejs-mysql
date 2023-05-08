a8c84bfb801df192e28cd27df75e8f39a8923b9b
NaijaTechBro <bakisodiq@gmail.com>
2023-05-08 13:48:09 +0100

    controller --product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,79 +1,79 @@
-const db = require('../models')
+// const db = require('../models')
 
-// create main Model
-const Product = db.products
-const Review = db.reviews
+// // create main Model
+// const Product = db.products
+// const Review = db.reviews
 
-// main work
+// // main work
 
-// create product
+// // create product
 
-const addProduct = async (req, res) => {
+// const addProduct = async (req, res) => {
 
-    let info = {
-        title: req.body.title,
-        price: req.body.price,
-        description: req.body.description,
-        published: req.body.published ? req.body.published : false
-    }
+//     let info = {
+//         title: req.body.title,
+//         price: req.body.price,
+//         description: req.body.description,
+//         published: req.body.published ? req.body.published : false
+//     }
 
-    const product = await Product.create(info)
-    res.status(200).send(product)
-}
+//     const product = await Product.create(info)
+//     res.status(200).send(product)
+// }
 
-// Get All Products
-const getAllProducts = async (req, res) => {
-    let products = await Product.findAll({})
-    res.status(200).send(products)
-}
+// // Get All Products
+// const getAllProducts = async (req, res) => {
+//     let products = await Product.findAll({})
+//     res.status(200).send(products)
+// }
 
-// Get Single Products
-const getOneProduct = async (req, res) => {
+// // Get Single Products
+// const getOneProduct = async (req, res) => {
     
-    let id = req.params.id
-    let products = await Product.findOne({ where: {id: id}})
-    res.status(200).send(products)
-}
+//     let id = req.params.id
+//     let products = await Product.findOne({ where: {id: id}})
+//     res.status(200).send(products)
+// }
 
-// Update product
+// // Update product
 
-const updateProduct = async (req, res) => {
+// const updateProduct = async (req, res) => {
 
-    let id = req.params.id
+//     let id = req.params.id
 
-    const product = await Product.update(req.body, { where: { id: id}})
+//     const product = await Product.update(req.body, { where: { id: id}})
 
-    res.status(200).send(product)
-}
+//     res.status(200).send(product)
+// }
 
-// Delete product
+// // Delete product
 
-const deleteProduct = async (req, res) => {
+// const deleteProduct = async (req, res) => {
 
-    let id = req.params.id
+//     let id = req.params.id
 
-    await Product.destroy({ where: { id: id}})
+//     await Product.destroy({ where: { id: id}})
 
-    res.status(200).send({ message: "Product is deleted!"})
-}
+//     res.status(200).send({ message: "Product is deleted!"})
+// }
 
-// Get Published product
+// // Get Published product
 
-const getPublishedProduct = async (req, res) => {
+// const getPublishedProduct = async (req, res) => {
 
-    const products = await Product.findAll({ where: { published: true}})
+//     const products = await Product.findAll({ where: { published: true}})
 
-    res.status(200).send(products)
-}
-
-
+//     res.status(200).send(products)
+// }
 
 
-module.exports = {
-    addProduct,
-    getAllProducts,
-    getOneProduct,
-    updateProduct,
-    deleteProduct,
-    getPublishedProduct,
-}
+
+
+// module.exports = {
+//     addProduct,
+//     getAllProducts,
+//     getOneProduct,
+//     updateProduct,
+//     deleteProduct,
+//     getPublishedProduct,
+// }
