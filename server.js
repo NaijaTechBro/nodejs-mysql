e55cc34f928f1fad0bbf84544db76af5581ce669
NaijaTechBro <bakisodiq@gmail.com>
2023-05-08 17:30:30 +0100

    server

--- server.js
+++ server.js
@@ -1,38 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-
-const app = express()
-
-const corOptions = {
-    origin: 'http://localhost:7890'
-}
-
-// routes
-
-const router = require('./routes/productRoutes')
-
-// middleware
-
-app.use(cors(corOptions))
-
-app.use(express.json())
-
-app.use(express.urlencoded({ extended: true}))
-
-app.use('/api/products', router)
-
-// Testing API
-app.get('/', (req, res) => {
-    res.json({ message: "hello from me"})
-})
-
-//PORT
-
-const PORT = process.env.PORT || 7890
+const cors = require('cors');
+const chalk = require('chalk');
+const app = require("./app");
+const PORT = process.env.PORT || PORT;
 
 // Server
 app.listen(PORT, () => {
-    console.log(`server is running on Port ${PORT}`)
+    console.log(chalk.green(`server is running on Port ${PORT}`))
 })
 
 
