2c3038696bb2d737c08207ae188729425cd44ac1
NaijaTechBro <bakisodiq@gmail.com>
2023-05-08 11:46:07 +0100

    controller --productw

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,4 +2,12 @@ const db = require('../models')
 
 // create main Model
 const Product = db.products
-const Review = db.re
+const Review = db.reviews
+
+// main work
+
+// create product
+
+const addProduct = async (req, res) => {
+    
+}
