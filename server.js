53c2a754eb2481d1067232a4932555ee971cf078
NaijaTechBro <bakisodiq@gmail.com>
2023-05-08 13:49:35 +0100

    server

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: true}))
 
-app.use('/api/products', router)
+// app.use('/api/products', router)
 
 // Testing API
 app.get('/', (req, res) => {
