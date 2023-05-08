6154dcabed1ae02f35c8140acbf0133cc54b9e97
NaijaTechBro <bakisodiq@gmail.com>
2023-05-08 13:48:54 +0100

    routes --product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,24 +1,24 @@
-const productController = require('../controllers/productController')
+// const productController = require('../controllers/productController')
 
-const router = require('express').Router()
+// const router = require('express').Router()
 
-router.post('/addProduct', productController.addProduct);
+// router.post('/addProduct', productController.addProduct);
 
-router.get('/allProducts', productController.getAllProducts);
+// router.get('/allProducts', productController.getAllProducts);
 
-router.get('/published', productController.getPublishedProduct);
+// router.get('/published', productController.getPublishedProduct);
 
-router.post('/addProduct', productController.addProduct)
-
-
-router.get('/getOneProduct/:id', productController.getOneProduct);
-
-router.put('/updateProduct/:id', productController.updateProduct);
-
-router.delete('/deleteProduct/:id', productController.deleteProduct);
+// router.post('/addProduct', productController.addProduct)
 
 
-module.exports = {
-    router
-}
+// router.get('/getOneProduct/:id', productController.getOneProduct);
+
+// router.put('/updateProduct/:id', productController.updateProduct);
+
+// router.delete('/deleteProduct/:id', productController.deleteProduct);
+
+
+// module.exports = {
+//     router
+// }
 
