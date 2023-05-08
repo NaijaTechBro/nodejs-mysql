ed31412e9a02783b07c8f92fd87eb7f58d62976b
NaijaTechBro <bakisodiq@gmail.com>
2023-05-08 11:05:11 +0100

    model --index

--- models/index.js
+++ models/index.js
@@ -7,6 +7,13 @@ const sequelize = new Sequelize(
     dbConfig.PASSWORD, {
         host: dbConfig.HOST,
         dialect: dbConfig.dialect,
-        operatorsAliases: false
+        operatorsAliases: false,
+
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle,
+        }
     }
 )
