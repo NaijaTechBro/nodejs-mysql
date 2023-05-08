1ead23be85a74b2b7378696805cfa8545aedb10e
NaijaTechBro <bakisodiq@gmail.com>
2023-05-08 17:31:30 +0100

    app

--- app.js
+++ app.js
@@ -8,53 +8,47 @@ const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 
-// Swagger UI
-const swaggerUI = require("swagger-ui-express");
-const YAML = require("yamljs");
-const swaggerJsDocs = YAML.load("./api.yaml");
-
 const app = express();
 const http = require('http');
 const path = require('path');
-const { logger, logEvents } = require('./middleware/logger');
-const errorHandler = require('./middleware/errorHandler');
-const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorMiddleware');
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 
 const server  = http.createServer(app);
 
-const connectDB = require('./config/dbConn');
+const corOptions = {
+    origin: 'http://localhost:7890'
+}
 
-// Route Import
-const subscriberRoutes = require("./routes/subscriberRoutes");
-const postRoutes = require("./routes/postRoutes");
-const adminRoutes = require('./routes/adminRoutes');
+// routes imports
+
+const router = require('./routes/productRoutes')
+
+// middleware
+
+app.use(express.json())
+app.use(logger)
+app.use(errorHandler)
+
+app.use(express.urlencoded({ extended: true}))
+
+// Routes
+app.use('/api/products', router)
+
+// Testing API
+app.get('/', (req, res) => {
+    res.json({ message: "hello from me"})
+})
+
 
 // Connecting to Database Environments
 console.log(chalk.redBright(process.env.NODE_ENV));
 
-connectDB()
-
-// Middlewares
-app.use(logger)
-app.use(errorHandler)
-
-
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+app.use(cors(corOptions));
 
-app.use((req, res, next,) => {
-    res.set({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-        "Content-Security-Policy": "default-src *",
-        "X-Content-Security-Policy": "default-src *",
-        "X-WebKit-CSP": "default-src *"
-    })
-    next();
-});
 
 app.use(express.json({ limit: "30mb", extended: true}))
 app.use(helmet());
@@ -63,15 +57,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ limit: "30mb", extended: false}))
 app.use(bodyParser.json())
 
-
-// Routes Middleware
-app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
-app.use("/api/v1/subscriber", subscriberRoutes);
-app.use("/api/v1/post", postRoutes);
-app.use("/api/v1/admin", adminRoutes);
-
-
-// Routes
+// Main Routes
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 app.all('*', (req, res) => {
